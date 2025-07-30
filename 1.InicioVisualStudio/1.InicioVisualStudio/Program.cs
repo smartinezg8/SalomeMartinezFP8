a2172f500f52f86cb98f6d6ff95d40262949d617
Salome Martinez <salome.martinezg@upb.edu.co>
2025-07-30 08:46:17 -0500

    Este es mi primer commit

---
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.InicioVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
