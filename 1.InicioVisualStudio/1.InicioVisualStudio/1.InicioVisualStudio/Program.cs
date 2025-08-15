3b39bd863c7cdd200412522642a0d898cb457903
smartinezg8 <salome.martinezg@upb.edu.co>
2025-08-15 08:59:43 -0500

    Este es mi primer commit

---
+++ 1.InicioVisualStudio/1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1.InicioVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración e inicialización de variables
+            string nombre="Salome";
+            const string iva = "19%";
+            string apellido = "Martinez Gallego";
+            //iva = "20%"; No puedo cambiar el valor de iva por ser una constante
+            Console.Write("Bienvenido al curso de Fundamentos de Programación\n");
+            nombre = "Tobby";
+            Console.Write("\t" + nombre + " " + apellido + " " + iva);
+            Console.ReadKey();
+        }
+    }
+}
