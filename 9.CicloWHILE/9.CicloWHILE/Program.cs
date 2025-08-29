3db5999c0939f897155211efed167ff90bede022
smartinezg8 <salome.martinezg@upb.edu.co>
2025-08-29 08:59:20 -0500

    Ciclo while ejercicio 1

---
+++ 9.CicloWHILE/9.CicloWHILE/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _9.CicloWHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+            Console.WriteLine("La suma de los primeros 5 números enteros es:" + acumulador);
+        }
+    }
+}
