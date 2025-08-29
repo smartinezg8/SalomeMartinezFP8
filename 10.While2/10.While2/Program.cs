2110f433afcc44e5df6e23be40c9b02f908bac3e
smartinezg8 <salome.martinezg@upb.edu.co>
2025-08-29 09:13:59 -0500

    While - Ejercicio 2

---
+++ 10.While2/10.While2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //10. Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1*2*3*4*5)
+            int num = 0;
+            int contador = 1;
+            int acumulador = 1;
+
+            Console.WriteLine("Escriba el número:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (contador <= num)
+            {
+                acumulador *= contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"El factorial de {num}: {acumulador}");
+        }
+    }
+}
