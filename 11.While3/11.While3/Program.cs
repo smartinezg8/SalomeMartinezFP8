6d0fe4e0f8dbb249a08e373280173d7d5a3d3ce8
smartinezg8 <salome.martinezg@upb.edu.co>
2025-08-29 09:55:13 -0500

    While - Ejercicio 3

---
+++ 11.While3/11.While3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _11.While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //11. Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados.
+
+            int acumulador = 0;
+            int numeros = 0; //Números es mi contador
+
+            Console.WriteLine("Ingrese los números a sumar, para finalizar ingrese un número negativo");
+
+
+            while (numeros >= 0)
+            {
+                acumulador += numeros;
+                Console.WriteLine($"Número: {numeros} Acumulador: {acumulador}");
+                numeros = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("La suma de los números enteros es:" + acumulador);
+        }
+    }
+}
