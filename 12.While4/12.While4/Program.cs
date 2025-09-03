59b29bdff201df0008db5452c5852d8fdf151fe9
smartinezg8 <salome.martinezg@upb.edu.co>
2025-09-03 08:24:24 -0500

    Ejercicio 4 de While

---
+++ 12.While4/12.While4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _12.While4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12.Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99. 
+            int contador = 1;
+            int acumulador = 0;
+            while (contador <= 99)
+            {
+                acumulador += contador;
+                Console.WriteLine($"{contador} - {acumulador}");
+                contador += 2;
+            }
+            Console.WriteLine($"La suma de los números impares hasta el 99 es: {acumulador}");
+        }
+    }
+}
