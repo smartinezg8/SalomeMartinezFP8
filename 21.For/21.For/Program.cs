2a275e04426423a8f963dd9c98b3ac4d14a598b2
smartinezg8 <salome.martinezg@upb.edu.co>
2025-09-17 08:09:08 -0500

    For

---
+++ 21.For/21.For/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _21.For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que genera y suma los 5 primeros números enteros
+            int acumulador = 0;
+            for (int i = 0; i <=5; i++)
+            {
+                acumulador += i;
+            }
+            Console.Write($"La suma de los cinco primeros números enteros es: {acumulador}");
+        }
+    }
+}
